e6041e9559f5453e0e610b042518f8efe55c9653
Ulrich Hecht <uli@suse.de>
2009-10-08 20:06:56 +0200

    do away with the FP crap

--- src/recompiler3/mips/arm_std_rec_calls.cpp
+++ src/recompiler3/mips/arm_std_rec_calls.cpp
@@ -2,14 +2,16 @@
 
 u32 psxBranchTest_rec(u32 cycles, u32 pc)
 {
+	int x;
 	/* Misc helper */
 	psxRegs->pc = pc;
-	psxRegs->cycle += (u32)( (float)(cycles) * BIAS_CYCLE_INC );
+	x = (u32)( (cycles) * 3 /* BIAS_CYCLE_INC */ );
+	psxRegs->cycle += x;
 
 	/* Make sure interrupts  always when mcd is active */
 	//if( mcdst != 0 || (psxRegs->cycle - psxRegs->psx_next_io_base)  >= psxRegs->psx_next_io_count )
 	{
-		update_hw((u32)( (float)(cycles) * BIAS_CYCLE_INC ));
+		update_hw((u32)( (cycles) * 3 /* BIAS_CYCLE_INC */ ));
 	}
 
 	u32 compiledpc = (u32)PC_REC32(psxRegs->pc);
